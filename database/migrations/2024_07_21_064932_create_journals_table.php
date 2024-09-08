5c0d4a27921c7ce56a471cc9975d9f7573984690
Marlo <111030422+Marlowkey@users.noreply.github.com>
2024-09-08 09:39:35 +0800

    feat: add journals table

--- database/migrations/2024_07_21_064932_create_journals_table.php
+++ database/migrations/2024_07_21_064932_create_journals_table.php
@@ -13,6 +13,11 @@ return new class extends Migration
     {
         Schema::create('journals', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+            $table->string('title');
+            $table->date('date');
+            $table->text('content');
+            $table->string('image_path')->nullable();
             $table->timestamps();
         });
     }
