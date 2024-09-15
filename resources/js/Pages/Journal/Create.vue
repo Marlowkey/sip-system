7156670030b7739de96e539d08b6ad16b8e7709a
Marlo <111030422+Marlowkey@users.noreply.github.com>
2024-09-15 18:49:00 +0800

    feat: add create view for journal

---
+++ resources/js/Pages/Journal/Create.vue
@@ -0,0 +1,113 @@
+<script setup>
+import { reactive, ref, computed } from 'vue'
+import { useForm } from '@inertiajs/vue3'
+import { mdiBallotOutline, mdiAccount, mdiMail, mdiFileDocumentAlertOutline   } from '@mdi/js'
+import SectionMain from '@/components/SectionMain.vue'
+import CardBox from '@/components/CardBox.vue'
+import FormCheckRadioGroup from '@/components/FormCheckRadioGroup.vue'
+import FormFilePicker from '@/components/FormFilePicker.vue'
+import FormField from '@/components/FormField.vue'
+import FormControl from '@/components/FormControl.vue'
+import BaseDivider from '@/components/BaseDivider.vue'
+import BaseButton from '@/components/BaseButton.vue'
+import BaseButtons from '@/components/BaseButtons.vue'
+import SectionTitle from '@/components/SectionTitle.vue'
+import LayoutAuthenticated from '@/layouts/LayoutAuthenticated.vue'
+import SectionTitleLineWithButton from '@/components/SectionTitleLineWithButton.vue'
+import NotificationBarInCard from '@/components/NotificationBarInCard.vue'
+import InputError from '@/components/InputError.vue'
+
+const props = defineProps({
+    journal: Object // Documents passed as a prop from Inertia
+})
+
+const journalId = ref(props.journal?.id);
+const isEditMode = computed(() => !!props.journal)
+const title = computed(() => isEditMode.value ? 'Edit Journal' : 'Create Journal')
+
+const form = useForm({
+    title: props.journal?.title || '',
+    date: props.journal?.date || '',
+    content: props.journal?.content || '',
+    image_path: null,
+})
+
+const submit = async () => {
+    try {
+        // Append file to FormData if it exists
+        const formData = new FormData()
+        formData.append('title', form.title)
+        formData.append('date', form.date)
+        formData.append('content', form.content)
+        if (form.image_path) {
+            formData.append('image_path', form. image_path)
+        }
+
+        if (isEditMode.value) {
+            await form.put(route('journals.update', documentId.value), formData)
+        } else {
+            await form.post(route('journals.store'), formData)
+        }
+        // Show success notification
+    } catch (error) {
+        console.error('Error submitting form:', error)
+        // Show error notification
+    }
+}
+
+const handleFileUpload = (event) => {
+    form.image_path = event.target.files[0]
+    form.image_path = file
+}
+
+const deleteJournal = async (journalId) => {
+    try {
+        const form = useForm({})
+        await form.delete(route('journals.destroy', documentId))
+        // Show success notification
+    } catch (error) {
+        console.error('Error deleting journal:', error)
+        // Show error notification
+    }
+}
+</script>
+<template>
+    <LayoutAuthenticated>
+        <SectionMain>
+            <SectionTitleLineWithButton :icon="mdiFileDocumentAlertOutline " :title="title" main>
+            </SectionTitleLineWithButton>
+            <CardBox isForm @submit.prevent="submit" enctype="multipart/form-data">
+                <FormField label="Title">
+                    <FormControl v-model="form.title" type="text" />
+                </FormField>
+                <InputError :message="form.errors.title" />
+
+                <FormField label="Date">
+                    <FormControl v-model="form.date" type="date" />
+                </FormField>
+                <InputError :message="form.errors.date" />
+
+                <BaseDivider />
+
+                <FormField label="Content" help="Describe the your experience.">
+                    <FormControl v-model="form.content" type="textarea" />
+                </FormField>
+                <InputError :message="form.errors.content" />
+
+                <FormField v-if="!isEditMode" label="Image" help="Upload an image file">
+                    <FormControl type="file" @change="handleFileUpload"/>
+                </FormField>
+                <InputError :message="form.errors.image_path" />
+
+                <template #footer>
+                    <BaseButtons>
+                        <BaseButton roundedFull small type="submit" color="blue" label="Submit" />
+                        <BaseButton roundedFull small type="reset" color="whiteTwo" label="Reset" v-if="!isEditMode" />
+                        <BaseButton roundedFull small color="red" label="Delete" @click.prevent="deleteJournal(journalId)"
+                            v-if="isEditMode" />
+                    </BaseButtons>
+                </template>
+            </CardBox>
+        </SectionMain>
+    </LayoutAuthenticated>
+</template>
